e6ba9e5dc81d7f9e64b97383fce6f8f28ee8883a
JIYHA <56276037+JIYHA@users.noreply.github.com>
2019-11-28 20:52:56 +0300

    Add files via upload

---
+++ Program3, laba 3.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace laba3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1, 2, 3, 4, 5");
+            int[] a ={ 1, 2, 3, 4, 5 };
+            int n = 5;
+            int k = Convert.ToInt32(Console.ReadLine());
+            if (k>5)
+                k %= 5;
+            for (int i = k; i < n; i++)
+            {
+                Console.Write("{0} ", a[i]);
+            }
+            for (int i = 0; i < k; i++)
+            {
+                Console.Write("{0} ", a[i]);
+            }
+        }
+    }
+}
