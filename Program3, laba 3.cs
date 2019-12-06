287d0a299643132399a549733a3a80cefd86bcd2
JIYHA <56276037+JIYHA@users.noreply.github.com>
2019-12-06 11:51:20 +0300

    Update Program3, laba 3.cs

--- Program3, laba 3.cs
+++ Program3, laba 3.cs
@@ -6,20 +6,23 @@ namespace laba3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("1, 2, 3, 4, 5");
-            int[] a ={ 1, 2, 3, 4, 5 };
-            int n = 5;
+            int[] a = { 1, 2, 3, 4, 5 };
+            Console.WriteLine("Enter you k");
             int k = Convert.ToInt32(Console.ReadLine());
-            if (k>5)
+            if (k > 5)
                 k %= 5;
-            for (int i = k; i < n; i++)
-            {
-                Console.Write("{0} ", a[i]);
-            }
             for (int i = 0; i < k; i++)
             {
-                Console.Write("{0} ", a[i]);
+                int tmp = a[0];
+                a[0] = a[1];
+                a[1] = a[2];
+                a[2] = a[3];
+                a[3] = a[4];
+                a[4] = tmp;
+                Console.Write("{0, 2}", a[i]);
             }
+            //Console.Write(a[]);
+            Console.ReadKey();
         }
     }
 }
