b9788661e727c54b3fff7f80b5087c056445cf46
JIYHA <56276037+JIYHA@users.noreply.github.com>
2019-12-13 12:38:48 +0300

    Update Program_ind.cs

--- Program_ind.cs
+++ Program_ind.cs
@@ -69,7 +69,60 @@ namespace Program7
             }
             //Console.Write("\n\n");
             Console.ReadKey();
+            
         }
     }   
 }
-    
+ //using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+
+//namespace ConsoleApplication1
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            Console.WriteLine("Pleas, weat");
+//            int[,] A = new int[9, 9];
+//            int N = 0;
+//            for ( int k = 1;k < 6; k++)
+//            {
+//                for (int i = -1+k; i<10-k; i++)
+//                {
+//                    A[i,-1+k]=N+1;
+//                    N += 1;
+//                    if (i==A.GetLength(0)-k)
+
+
+//                    for (int j = -1+k; j<10-k; j++)
+//                    {
+//                        A[9-k,j]=N + 1;
+//                        N += 1;
+//                        if (j==A.GetLength(1)-k)
+
+
+//                            for (i = A.GetLength(0) - k; i > -1 + k; i--)
+//                            {
+//                                A[i, -1 + k] = N + 1;
+//                                N += 1;
+//                                if ( i==-1+k)
+
+
+//                                    for (j = A.GetLength(1) - k; j > k; j--)
+//                                    {
+//                                        A[9 - k, j] = N + 1;
+//                                        N += 1;
+//                                        Console.Write("{0,3}", A[i, j]);
+//                                    }
+//                            }
+//                    }
+ 
+//                }
+//            }
+//            Console.ReadKey();
+        
+//        }
+//    }
+//}   
